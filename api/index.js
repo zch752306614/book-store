71e3ce0dbc8c0c0db5cf0d81af6b4b8593d41a2d
ZhangChenhuang <752306614@qq.com>
2024-01-09 18:51:48 +0800

    fix: 修改接口地址

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 import * as API from './base.js'
 export default {
-	// 获取小说列表
-	getBookList (params, nohide, noloading) {
-		return API.GET(`/api/novelManage/queryNovelist`, params, nohide, noloading)
-	},
-	// 获取小说章节
-	getBook (params, nohide, noloading) {
-		return API.GET(`/api/novelManage/queryChapterList`, params, nohide, noloading)
-	},
-	// 根据id获取小说章节
-	getBookById (params, nohide, noloading) {
-		return API.GET(`/api/novelManage/queryChapterListById`, params, nohide, noloading)
-	}
+    // 获取小说列表
+    getBookList(params, nohide, noloading) {
+        return API.GET(`/api/novelManage/queryNovelist`, params, nohide, noloading)
+    },
+    // 获取小说章节
+    getBook(params, nohide, noloading) {
+        return API.GET(`/api/novelManage/queryChapterList`, params, nohide, noloading)
+    },
+    // 根据id获取小说章节
+    getBookById(params, nohide, noloading) {
+        return API.GET(`/api/novelManage/queryChapterListById`, params, nohide, noloading)
+    }
 }
